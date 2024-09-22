702f78634f3e9a9467720ac2c594ac26e7c08cc1
kvrivera <heykatrivera@gmail.com>
2024-09-21 23:03:51 -0400

    Fixed validation errors on Week 1 Homepage Assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -12,6 +12,6 @@ let lastMod = document.querySelector("#lastModified");
 // convert the lastModified property into a date object:
 const dateLastMod = new Date();
 // pick out only the Month, date, year, hours, mins, seconds
-document.getElementById("lastModified").innerHTML = dateLastMod
+document.getElementById("lastModified").innerHTML = `Last Modified: <span class="lastMod">${dateLastMod}</span>`
 // why does this next line not work, instead of the line above?
 //lastMod.innerHTML = `<span class="lastMod">${dateLastMod.getFull()}</span>`
