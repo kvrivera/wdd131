ed9cca8c49ff233fa25de8ee8611129fd1586425
kvrivera <heykatrivera@gmail.com>
2024-10-25 20:21:28 -0400

    js code for filtering storybooks

---
+++ project/scripts/project-base.js
@@ -0,0 +1,174 @@
+// more than one function
+// Include DOM interaction: select an element, modify it, and listen for and react to events.
+// Use conditional branching.
+// Use objects, arrays, and array methods.
+// Exclusively use template literals when building strings.
+// Use localStorage.
+
+// storybook array
+// themes: resilience, emotional regulation, death, goodbye, be yourself
+const storybooks = [
+    {
+        bookTitle: "When I Feel Sad",
+        author: "Paula Bowles",
+        illustrator: "",
+        themes: "resilience, emotional regulation",
+        myRating: 5,
+        imgLocation: "images/when-i-feel-sad.jpg",
+        imgAltText: "Cover of When I Feel Sad by Paula Bowles"
+    },
+    {
+        bookTitle: "I Wish I Could Tell You",
+        author: "Jean-François Sénéchal",
+        illustrator: "",
+        themes: "death",
+        myRating: 5,
+        imgLocation: "images/i-wish-i-could-tell-you.jpg",
+        imgAltText: "Cover of I Wish I Could Tell You by Jean-François Sénéchal"
+    },
+    {
+        bookTitle: "In a Jar",
+        author: "Deborah Marcero",
+        illustrator: "",
+        themes: "goodbye",
+        myRating: 5,
+        imgLocation: "images/in-a-jar.jpg",
+        imgAltText: "Cover of In a Jar by Deborah Marcero"
+    },
+    {
+        bookTitle: "Little Wolf's First Howling",
+        author: "Laura McGee Kvasnosky",
+        illustrator: "",
+        themes: "be yourself",
+        myRating: 5,
+        imgLocation: "images/little-wolfs-first-howling.jpg",
+        imgAltText: "Cover of Little Wolf's First Howling by Laura McGee Kvasnosky"
+    },
+    {
+        bookTitle: "The Rescuer of Tiny Creatures",
+        author: "Curtis Manley",
+        illustrator: "",
+        themes: "be yourself",
+        myRating: 5,
+        imgLocation: "images/the-rescuer-of-tiny-creatures.jpg",
+        imgAltText: "Cover of The Rescuer of Tiny Creatures by Curtis Manley"
+    },
+    {
+        bookTitle: "Not Quite Narwhal",
+        author: "Jessie Sima",
+        illustrator: "",
+        themes: "be yourself",
+        myRating: 5,
+        imgLocation: "images/not-quite-narwhal.jpg",
+        imgAltText: "Cover of Not Quite Narwhal by Jessie Sima"
+    },
+    {
+        bookTitle: "It's Okay To Be a Unicorn",
+        author: "Jason Tharp",
+        illustrator: "",
+        themes: "be yourself",
+        myRating: 5,
+        imgLocation: "images/its-okay-to-be-a-unicorn.jpg",
+        imgAltText: "Cover of It's Okay to be a Unicorn by Jason Tharp"
+    },
+    {
+        bookTitle: "Sensitive",
+        author: "Sara Levine",
+        illustrator: "",
+        themes: "emotional regulation",
+        myRating: 5,
+        imgLocation: "images/sensitive.jpg",
+        imgAltText: "Cover of Sensitive by Sara Levine"
+    },
+    {
+        bookTitle: "Daisy",
+        author: "Jessixa Bagley",
+        illustrator: "",
+        themes: "be yourself",
+        myRating: 5,
+        imgLocation: "images/daisy.jpg",
+        imgAltText: "Cover of Daisy by Jessixa Bagley"
+    },
+    {
+        bookTitle: "Walrus in the Bathtub",
+        author: "",
+        illustrator: "",
+        themes: "change",
+        myRating: 5,
+        imgLocation: "images/walrus-in-the-bathtub.jpg",
+        imgAltText: "Cover of Walrus in the Bathtub by "
+    },
+    {
+        bookTitle: "Maple and Willow Apart",
+        author: " ",
+        illustrator: "",
+        themes: "change",
+        myRating: 5,
+        imgLocation: "images/maple-and-willow-apart.jpg",
+        imgAltText: "Cover of Maple and Willow Apart by "
+    },
+    {
+        bookTitle: "Little Tree",
+        author: " ",
+        illustrator: "",
+        themes: "change",
+        myRating: 5,
+        imgLocation: "images/little-tree.jpg",
+        imgAltText: "Cover of Little Tree by "
+    }
+]
+const storybookHeading = document.querySelector(".storybook-imgs h3")
+const storybookCards = document.querySelector(".storybook-imgs");
+// create function to create storybook cards
+function createStorybookCards(storybooks) {
+    storybookCards.innerHTML = ""; // clear any existing content
+    storybooks.forEach(function (storybook) { // like saying "for each storybook in storybooks"
+        const card = document.createElement("figure"); // creature a figure
+        card.innerHTML = `
+            <h4>${storybook.bookTitle}</h4>
+            <p>Author: ${storybook.author}</p>
+            <p>My Rating: ${storybook.myRating} ⭐️</p>
+            <p>Themes: ${storybook.themes}</p>
+            <img src="${storybook.imgLocation}" alt="${storybook.imgAltText}" loading="lazy">
+        `
+        storybookCards.appendChild(card); // append it so it shows up on the html doc
+    })
+}
+
+// now call the function to create the intitial storybook cards
+createStorybookCards(storybooks);
+
+const resilienceLink = document.querySelector("#resiliencelink");
+// when user clicks resilience
+
+// when user clicks regulation
+const regulationLink = document.querySelector("#regulationlink");
+regulationLink.addEventListener("click", () => {
+    const regulationStorybooks = storybooks.themes.filter(storybook => storybook.themes.includes("regulation"));
+    createStorybookCards(regulationStorybooks);
+    storybookHeading.innerText = "Stories About Resilience";
+})
+
+// when user clicks death
+const deathLink = document.querySelector("#deathlink");
+deathLink.addEventListener("click", () => {
+    const deathStorybooks = storybooks.themes.filter.includes("death");
+    storybookHeading.innerText = "Stories About Death";
+})
+
+// when user clicks goodbyes
+const goodbyeLink = document.querySelector("#goodbyeslink");
+goodbyeLink.addEventListener("click", () => {
+    const goodbyeStorybooks = storybooks.themes.filter.includes("goodbye");
+    storybookHeading.innerText = "Stories About Death";
+})
+
+// when user clicks be yourself
+const beYourselfLink = document.querySelector("#beyourselflink");
+beYourselfLink.addEventListener("click", () => {
+    const beYourselfStorybooks = storybooks.themes.filter.includes("be yourself");
+    storybookHeading.innerText = "Stories About Being Yourself";
+})
+
+
+
